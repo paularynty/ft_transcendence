5af4c4f081e703036bb3f0cd4525d078d417f22a
Paula Rynty <paula.rynty@gmail.com>
2025-08-16 00:12:38 +0300

    adding initial styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <a className="link" href="/">
+        Pong!
+      </a>
+    </header>
+  );
+};
